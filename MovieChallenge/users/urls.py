a508d3f3c1e9e0836bcbe81c85247e2631ef6ebf
sabu-aka <jiuzhiyong88@gmail.com>
2024-09-05 03:36:03 +0900

    Update urls.py
    パス設定修正

--- MovieChallenge/users/urls.py
+++ MovieChallenge/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SignupView
+from .views import SignupView 
+from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='users/logout.html'), name='logout'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('home/',views.home, name="home"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
 ]
